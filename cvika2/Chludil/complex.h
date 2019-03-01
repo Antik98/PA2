47ed28b1e63217581a6736f6824253d2c6998c47
Pham Minh Duc <phamducm@fit.cvut.cz>
2019-03-01 13:12:34 +0100

    cvika chludil

---
+++ cvika2/Chludil/complex.h
@@ -0,0 +1,11 @@
+//
+// Created by phamducm on 3/1/19.
+//
+
+#include <iostream>
+using namespace std;
+class Complex{
+    imaginarni a realna slozka
+    konstruktor
+
+};
--- cvika2/Chludil/main.cpp
+++ cvika2/Chludil/main.cpp
@@ -2,3 +2,15 @@
 // Created by phamducm on 3/1/19.
 //
 
+#include <iostrea>
+#include <csdlib>
+#include "complex.h"
+
+
+using namespace std;
+int main (){
+    Complex A();
+    Complex B(1.5, 2.78);
+    Complex C(1);
+
+}
