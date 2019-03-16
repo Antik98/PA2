bc9513b55b506a13ac2cbf8cc63c6971442f8f7a
Antik98 <antichristal@DESKTOP-JN37PUN.localdomain>
2019-03-17 00:20:11 +0100

    Progtest finished?

--- progtest1/main.cpp
+++ progtest1/main.cpp
@@ -30,6 +30,7 @@ const uint16_t ENDIAN_BIG = 0x4d4d;
 #endif /* __PROGTEST__ */
 class CImage{
 public:
+    CImage();
     int vyska;
     int sirka;
     enum endian_ {littleEndian, bigEndian};
@@ -39,8 +40,15 @@ public:
     vector<vector<uint64_t>> pole;
     uint16_t hlavicka[8];
 };
-
-
+CImage::CImage()
+        :vyska(0),
+         sirka(0),
+         kanal(0),
+         bPerKanal(0),
+         endian(littleEndian){
+    for (int i = 0; i < 8 ; i++)
+        hlavicka[i]=0;
+}
 void printImg(const CImage imgFile){
     int tmp=0;
     for(int i =0 ; i<imgFile.vyska;i++){
@@ -52,38 +60,18 @@ void printImg(const CImage imgFile){
             else{
                 putchar(' ');
             }
-
         }
     }
 }
-bool readFile(CImage &imgFile, string fileName){
+bool readFile(CImage &imgFile, const char* fileName){
     ifstream in;
     in.open(fileName,ios::binary);
 
     if (in.is_open()){
 
-        //get the starting position
-        streampos start = in.tellg();
+        in.read((char*)&imgFile.hlavicka[0],8); // Reading 8 bytes of header
 
-        // go to the end
-        in.seekg(0, ios::end);
-
-        //get the ending position
-        streampos end = in.tellg();
-
-        //go back to the start
-        in.seekg(0, ios::beg);
-
-        //create a vecotr to hold the data that is resized to the total size of the file
-        vector<char> contents;
-        //read it in
-        in.read((char*)&imgFile.hlavicka[0],8);
-
-        int l=0;
-        int tmp =0; //prvni 8 bitu pryc
-
-        cout << "Size of image :" << dec << (end - start)-8 << endl;
-        cout << "Type of endian: ";
+        cout << "Type of endian: ";                     // Parsing inputs of header
         if(imgFile.hlavicka[0] == ENDIAN_LITTLE) {
             cout << "Little Endian" << endl;
             imgFile.endian = CImage::littleEndian;
@@ -102,14 +90,10 @@ bool readFile(CImage &imgFile, string fileName){
             imgFile.sirka=imgFile.hlavicka[1]<<8;
             imgFile.vyska=imgFile.hlavicka[2]<<8;
         }
+        cout << "Size of image :" << dec << imgFile.sirka*imgFile.vyska << endl;
         cout << "Sirka : " << imgFile.sirka << endl;
         cout << "Vyska: " << imgFile.vyska<< endl;
 
-        //check if Size image == size specified
-        if(imgFile.sirka*imgFile.vyska!= (end - start)-8){
-            cout<< "Error: image je vetsi/mensi nez v hlavicce"<< endl;
-            return false;
-        }
 
         imgFile.kanal=imgFile.hlavicka[3] & 0b11;
         imgFile.bPerKanal=(imgFile.hlavicka[3] & 0b11100)>>2;
@@ -117,7 +101,7 @@ bool readFile(CImage &imgFile, string fileName){
             cout<< "spatna imgFile.hlavicka";
             return false;
         }
-
+        //Setting input as readable && ruling out invalid inputs
         switch(imgFile.kanal){
             case 0b00:
                 imgFile.kanal=1;
@@ -146,95 +130,100 @@ bool readFile(CImage &imgFile, string fileName){
                 cout<<"Chyba v bPerKanal" << endl;
                 return false;
         }
+
         cout<< "Kanal: " << imgFile.kanal << endl;
         cout<< "bPerKanal: "<< imgFile.bPerKanal << endl;
 
-        in.seekg(8,ios::beg);
-        contents.resize(static_cast<size_t>((end - start)-8));
-        in.read(&contents[0], contents.size());
 
         imgFile.pole.resize(static_cast<uint64_t>(imgFile.vyska));//resizing based on picture
         for(int i=0;i<imgFile.vyska;i++){
             imgFile.pole[i].resize(static_cast<uint64_t>(imgFile.sirka));
         }
-        
-        if(imgFile.bPerKanal==8){
-        }
-        in.seekg(8,ios::beg);
+
+        in.seekg(8,ios::beg);                                // setting cursor after header
         for(int i =0 ; i<imgFile.vyska;i++){
             for(int l=0;l<imgFile.sirka;l++){
-                in.read((char*)&imgFile.pole[i][l],imgFile.bPerKanal/8*imgFile.kanal);
+                if(!in.read((char*)&imgFile.pole[i][l],imgFile.bPerKanal/8*imgFile.kanal)){ // reading binary file into 2D array
+                    return false;
+                }
             }
         }
-        //printImg(imgFile);
 
-       /* putchar('\n');
-        for(const char& c: contents){
-            //printbinchar(c);
-            cout<<setw(2) << setfill('0') << hex<<(int)c;
-            l++;
-            tmp++;
-            if(!(l%imgFile.sirka)) {
-                putchar('\n');
-            }
-            else{
-                putchar(' ');
-            }
-
-        }*/
+        uint64_t tmpPixel; // checking for bytes of full read, potential error
+        if(in.read((char*)&tmpPixel,1)){
+            cout<< "Nasel se bit prevysujici pole"<< endl;
+            return false;
+        }
     }
     in.close();
     return true;
-
 }
 
-void flipH(CImage *imgFile){
-    cout<< "Pred flipem : "<< endl;
+bool flipH(CImage *imgFile){
+    /*cout<< "Pred flipem : "<< endl;   // debugging, printing file
     printImg(*imgFile);
-    /*for (int x = 0; x < imgFile->sirka/2; x++) {
-        for (int y = 0; y < imgFile->vyska; y++) {
-            uint64_t temp = imgFile->pole[x][y];
-            imgFile->pole[x][y]=imgFile->pole[imgFile->sirka-x][y];
-            imgFile->pole[imgFile->sirka-x][y]=temp;
-        }
-    }*/
-    for(int x = 0;x<imgFile->vyska/2;x++){
-        for (int y = 0; y< imgFile->sirka;y++){
-            uint64_t  temp = imgFile->pole[x][y];
-            imgFile->pole[x][y]=imgFile->pole[imgFile->vyska-x-1][y];
-            imgFile->pole[imgFile->vyska-x-1][y]=temp;
+    */
+    int sirkaFlip=imgFile->sirka-1;
+    int vyskaFlip= imgFile->vyska-1;
+    if (sirkaFlip<0 || vyskaFlip<0){
+        cout << "Error vyskaflip blbe" << endl;// potential error, perhaps img could be very small
+        return false;
+    }
+
+    for (int y = 0; y <imgFile->vyska; y++) {
+        for (int x = 0; x < imgFile->sirka/2; x++) {
+            uint64_t temp = imgFile->pole[y][x];
+            imgFile->pole[y][x]=imgFile->pole[y][sirkaFlip-x];
+            imgFile->pole[y][sirkaFlip-x]=temp;
         }
     }
-    cout<< "Po flipu : "<< endl;
-    printImg(*imgFile);
 
-    cout<< "reference: "<< endl;
+    /*cout<< endl<< "Po flipu : "<< endl;
+    printImg(*imgFile);
+    cout<< endl<< "reference: "<< endl;
+
     CImage tmp;
     readFile(tmp, "ref_00.img");
-    printImg(tmp);
+    printImg(tmp);*/
 
+    return true;
 }
-void flipV(CImage *imgFile){
-    for (int x = 0; x < imgFile->sirka; x++) {
-        for (int y = 0; y < imgFile->vyska/2; y++) {
-            uint64_t temp = imgFile->pole[x][y];
-            imgFile->pole[x][y]=imgFile->pole[x][imgFile->vyska-y];
-            imgFile->pole[x][imgFile->vyska-y]=temp;
+bool flipV(CImage *imgFile){
+
+    int sirkaFlip=imgFile->sirka-1;
+    int vyskaFlip= imgFile->vyska-1;
+    if (sirkaFlip<0 || vyskaFlip<0){
+        cout << "Error vyskaflip blbe" << endl;// potential error, perhaps img could be very small
+        return false;
+    }
+
+    for (int y = 0; y < imgFile->vyska/2; y++) {
+        for (int x = 0; x < imgFile->sirka; x++) {
+            uint64_t temp = imgFile->pole[y][x];
+            imgFile->pole[y][x]=imgFile->pole[vyskaFlip-y][x];
+            imgFile->pole[vyskaFlip-y][x]=temp;
         }
     }
+    return true;
 }
-void saveFile(CImage *imgFile, string dstFileName){
+bool saveFile(CImage *imgFile, const char *dstFileName){
     ofstream outFile;
-    outFile.open(dstFileName);
-    for(int i =0 ; i<8;i++){
-        outFile<<imgFile->hlavicka[i];
+    outFile.open(dstFileName, ios::binary|ios::out);
+    if(outFile.fail()) // possible not able to create file
+        return false;
+
+    for(int i =0 ; i< 4;i++) {
+        outFile.write((char *) &imgFile->hlavicka[i], 2); // writing header
     }
     for(int i =0 ; i<imgFile->vyska;i++){
         for(int l=0;l<imgFile->sirka;l++){
-            outFile<<imgFile->pole[i][l];
+            outFile.write((char*)&imgFile->pole[i][l], imgFile->kanal*imgFile->bPerKanal/8); // writing rest of image
         }
     }
+    outFile.close();
+    return true;
 }
+
 bool flipImage(const char *srcFileName,
                const char *dstFileName,
                bool flipHorizontal,
@@ -245,26 +234,17 @@ bool flipImage(const char *srcFileName,
         return false;
     }
     if(flipHorizontal && !flipVertical){
-        flipH(&imgFile);
-        saveFile(&imgFile,dstFileName);
+        if(!flipH(&imgFile)) return false;
     }else if (!flipHorizontal && flipVertical){
-        flipV(&imgFile);
-        saveFile(&imgFile,dstFileName);
+        if(!flipV(&imgFile)) return false;
     }else if(flipHorizontal && flipVertical){
-       flipH(&imgFile);
-       flipV(&imgFile);
-       saveFile(&imgFile,dstFileName);
-    }else
-        return false;
-    return true;
+        if(!flipH(&imgFile)) return false;
+        if(!flipV(&imgFile)) return false;
+    }
+
+    return (saveFile(&imgFile,dstFileName));
 }
 
-void printbinchar(char c){
-    for(int i=0; i<=5; i++){
-        putchar( (1 & (c >> i)) ? '1' : '0' );
-    }
-    putchar(' ');
-}
 #ifndef __PROGTEST__
 
 bool identicalFiles(const char *fileName1,
@@ -283,61 +263,59 @@ bool identicalFiles(const char *fileName1,
             }
         }
     }
-
     return true;
 }
 
 
 int main(void) {
-    flipImage ( "input_00.img", "output_00.img", true, false );
 
-     //assert ( flipImage ( "input_00.img", "output_00.img", true, false )
-     //         && identicalFiles ( "output_00.img", "ref_00.img" ) );
+    assert ( flipImage ( "input_00.img", "output_00.img", true, false )
+             && identicalFiles ( "output_00.img", "ref_00.img" ) );
 
-     /*assert ( flipImage ( "input_01.img", "output_01.img", false, true )
-              && identicalFiles ( "output_01.img", "ref_01.img" ) );
-     assert ( flipImage ( "input_02.img", "output_02.img", true, true )
-              && identicalFiles ( "output_02.img", "ref_02.img" ) );
-     assert ( flipImage ( "input_03.img", "output_03.img", false, false )
-              && identicalFiles ( "output_03.img", "ref_03.img" ) );
-     assert ( flipImage ( "input_04.img", "output_04.img", true, false )
-              && identicalFiles ( "output_04.img", "ref_04.img" ) );
-     assert ( flipImage ( "input_05.img", "output_05.img", true, true )
-              && identicalFiles ( "output_05.img", "ref_05.img" ) );
-     assert ( flipImage ( "input_06.img", "output_06.img", false, true )
-              && identicalFiles ( "output_06.img", "ref_06.img" ) );
-     assert ( flipImage ( "input_07.img", "output_07.img", true, false )
-              && identicalFiles ( "output_07.img", "ref_07.img" ) );
-     assert ( flipImage ( "input_08.img", "output_08.img", true, true )
-              && identicalFiles ( "output_08.img", "ref_08.img" ) );
-     assert ( ! flipImage ( "input_09.img", "output_09.img", true, false ) );
-     */
+    assert ( flipImage ( "input_01.img", "output_01.img", false, true )
+             && identicalFiles ( "output_01.img", "ref_01.img" ) );
+    assert ( flipImage ( "input_02.img", "output_02.img", true, true )
+             && identicalFiles ( "output_02.img", "ref_02.img" ) );
+    assert ( flipImage ( "input_03.img", "output_03.img", false, false )
+             && identicalFiles ( "output_03.img", "ref_03.img" ) );
+    assert ( flipImage ( "input_04.img", "output_04.img", true, false )
+             && identicalFiles ( "output_04.img", "ref_04.img" ) );
+    assert ( flipImage ( "input_05.img", "output_05.img", true, true )
+             && identicalFiles ( "output_05.img", "ref_05.img" ) );
+    assert ( flipImage ( "input_06.img", "output_06.img", false, true )
+             && identicalFiles ( "output_06.img", "ref_06.img" ) );
+    assert ( flipImage ( "input_07.img", "output_07.img", true, false )
+             && identicalFiles ( "output_07.img", "ref_07.img" ) );
+    assert ( flipImage ( "input_08.img", "output_08.img", true, true )
+             && identicalFiles ( "output_08.img", "ref_08.img" ) );
+    assert ( ! flipImage ( "input_09.img", "output_09.img", true, false ) );
 
-     /*// extra inputs (optional & bonus tests)
-     assert ( flipImage ( "extra_input_00.img", "extra_out_00.img", true, false )
-              && identicalFiles ( "extra_out_00.img", "extra_ref_00.img" ) );
-     assert ( flipImage ( "extra_input_01.img", "extra_out_01.img", false, true )
-              && identicalFiles ( "extra_out_01.img", "extra_ref_01.img" ) );
-     assert ( flipImage ( "extra_input_02.img", "extra_out_02.img", true, false )
-              && identicalFiles ( "extra_out_02.img", "extra_ref_02.img" ) );
-     assert ( flipImage ( "extra_input_03.img", "extra_out_03.img", false, true )
-              && identicalFiles ( "extra_out_03.img", "extra_ref_03.img" ) );
-     assert ( flipImage ( "extra_input_04.img", "extra_out_04.img", true, false )
-              && identicalFiles ( "extra_out_04.img", "extra_ref_04.img" ) );
-     assert ( flipImage ( "extra_input_05.img", "extra_out_05.img", false, true )
-              && identicalFiles ( "extra_out_05.img", "extra_ref_05.img" ) );
-     assert ( flipImage ( "extra_input_06.img", "extra_out_06.img", true, false )
-              && identicalFiles ( "extra_out_06.img", "extra_ref_06.img" ) );
-     assert ( flipImage ( "extra_input_07.img", "extra_out_07.img", false, true )
-              && identicalFiles ( "extra_out_07.img", "extra_ref_07.img" ) );
-     assert ( flipImage ( "extra_input_08.img", "extra_out_08.img", true, false )
-              && identicalFiles ( "extra_out_08.img", "extra_ref_08.img" ) );
-     assert ( flipImage ( "extra_input_09.img", "extra_out_09.img", false, true )
-              && identicalFiles ( "extra_out_09.img", "extra_ref_09.img" ) );
-     assert ( flipImage ( "extra_input_10.img", "extra_out_10.img", true, false )
-              && identicalFiles ( "extra_out_10.img", "extra_ref_10.img" ) );
-     assert ( flipImage ( "extra_input_11.img", "extra_out_11.img", false, true )
-              && identicalFiles ( "extra_out_11.img", "extra_ref_11.img" ) );*/
+
+    /*// extra inputs (optional & bonus tests)
+    assert ( flipImage ( "extra_input_00.img", "extra_out_00.img", true, false )
+             && identicalFiles ( "extra_out_00.img", "extra_ref_00.img" ) );
+    assert ( flipImage ( "extra_input_01.img", "extra_out_01.img", false, true )
+             && identicalFiles ( "extra_out_01.img", "extra_ref_01.img" ) );
+    assert ( flipImage ( "extra_input_02.img", "extra_out_02.img", true, false )
+             && identicalFiles ( "extra_out_02.img", "extra_ref_02.img" ) );
+    assert ( flipImage ( "extra_input_03.img", "extra_out_03.img", false, true )
+             && identicalFiles ( "extra_out_03.img", "extra_ref_03.img" ) );
+    assert ( flipImage ( "extra_input_04.img", "extra_out_04.img", true, false )
+             && identicalFiles ( "extra_out_04.img", "extra_ref_04.img" ) );
+    assert ( flipImage ( "extra_input_05.img", "extra_out_05.img", false, true )
+             && identicalFiles ( "extra_out_05.img", "extra_ref_05.img" ) );
+    assert ( flipImage ( "extra_input_06.img", "extra_out_06.img", true, false )
+             && identicalFiles ( "extra_out_06.img", "extra_ref_06.img" ) );
+    assert ( flipImage ( "extra_input_07.img", "extra_out_07.img", false, true )
+             && identicalFiles ( "extra_out_07.img", "extra_ref_07.img" ) );
+    assert ( flipImage ( "extra_input_08.img", "extra_out_08.img", true, false )
+             && identicalFiles ( "extra_out_08.img", "extra_ref_08.img" ) );
+    assert ( flipImage ( "extra_input_09.img", "extra_out_09.img", false, true )
+             && identicalFiles ( "extra_out_09.img", "extra_ref_09.img" ) );
+    assert ( flipImage ( "extra_input_10.img", "extra_out_10.img", true, false )
+             && identicalFiles ( "extra_out_10.img", "extra_ref_10.img" ) );
+    assert ( flipImage ( "extra_input_11.img", "extra_out_11.img", false, true )
+             && identicalFiles ( "extra_out_11.img", "extra_ref_11.img" ) );*/
     return 0;
 }
 
