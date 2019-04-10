aa2ad21bf815bbab117c1628a0d528daa101b4a1
Pham Minh Duc <phamducm@fit.cvut.cz>
2019-04-10 12:00:29 +0200

    initial prog5

---
+++ progtest5/cmake-build-debug/CMakeFiles/progtest5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/progtest5.dir/main.cpp.o"
+  "progtest5.pdb"
+  "progtest5"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/progtest5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
