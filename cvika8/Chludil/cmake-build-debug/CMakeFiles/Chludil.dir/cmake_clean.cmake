88cfa76b328ae1a4f4f10158978a983834528749
Pham Minh Duc <phamducm@fit.cvut.cz>
2019-04-12 13:04:34 +0200

    school pc

---
+++ cvika8/Chludil/cmake-build-debug/CMakeFiles/Chludil.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chludil.dir/main.cpp.o"
+  "Chludil.pdb"
+  "Chludil"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chludil.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
